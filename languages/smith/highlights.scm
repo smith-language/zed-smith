a8e90aa4f8caa5bc396f399bc484ae891ed80bff
Adam Kowalski <adam.kowalski.work@gmail.com>
2025-03-22 21:40:05 -0700

    highlight struct

--- languages/smith/highlights.scm
+++ languages/smith/highlights.scm
@@ -1,4 +1,61 @@
+(type_identifier) @type
+
+(primitive_type) @type.builtin
+
+(identifier) @variable
+
+(anonymous) @variable
+
+(function_definition
+    name: (identifier) @function.name)
+
+(call_expression
+  function: (identifier) @function.call)
+
+(integer_literal) @number
+
+(boolean_literal) @boolean
+
+"struct" @keyword.class
+
+"fn" @keyword.function
+
+"for" @keyword.repeat
+
 [
-  "fn"
   "let"
 ] @keyword
+
+[
+    (or)
+    (and)
+    (equal)
+    (not_equal)
+    (less)
+    (less_equal)
+    (greater)
+    (greater_equal)
+    (add)
+    (subtract)
+    (multiply)
+    (divide)
+    (modulo)
+    (negative)
+    (not)
+    "="
+] @operator
+
+[
+  "("
+  ")"
+  "["
+  "]"
+  "{"
+  "}"
+] @punctuation.bracket
+
+[
+  ","
+  ":"
+  "->"
+] @punctuation.delimiter
